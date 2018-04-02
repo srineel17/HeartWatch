c0b74c33fec61cbe705f05921888084e005d63cc
Srineel Soni <srineel2052@gmail.com>
2018-04-02 17:51:39 +0530

    a

--- heart_attack/views.py
+++ heart_attack/views.py
@@ -23,12 +23,26 @@ def insert_user(request):
 	form = InsertForm(request.POST or None)
 
 	if form.is_valid():
+
 		fullname = request.POST['fullname']
 		age = request.POST['age']
 		sex = request.POST['sex']
 		cp_type = request.POST['cp_type']
 		trest_bps =request.POST['trest_bps']
+
+		# if trest_bps<90 or trest_bps>210:
+		# 	context = {
+        #         'form': form,
+        #         'error_message': 'Please Read description and enter correct Trest_bps value',
+        #     }
+		# 	return render(request, 'heart_attack/index.html',context)
 		chol = request.POST['chol']
+		# if chol not in range(100,570):
+		# 	context = {
+        #         'form': form,
+        #         'error_message': 'Please Read description and enter correct Cholesterol value',
+        #     }
+		# 	return render(request, 'heart_attack/index.html',context)
 		fbs = request.POST['fbs']
 		rer = request.POST['rer']
 		thalch = request.POST['thalch']
@@ -48,7 +62,17 @@ def insert_user(request):
 		context = {
 	    	"to_print": int(to_print),
 	    }
-		return render(request, 'heart_attack/result.html',context)
+
+		if to_print == 0:
+			return render(request, 'heart_attack/result0.html',context)
+		elif to_print == 1:
+			return render(request, 'heart_attack/result1.html',context)
+		elif to_print == 2:
+			return render(request, 'heart_attack/result2.html',context)
+		elif to_print == 3:
+			return render(request, 'heart_attack/result3.html',context)
+		elif to_print == 4:
+			return render(request, 'heart_attack/result4.html',context)
 
 	context = {
     	"form": form,
@@ -76,10 +100,10 @@ def c_predict(a_list):
     value.append(f_list)
 
     modelSVM = LinearSVC(C=0.001)
-    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.001 ,train_size=0.999, random_state=0)
-    modelSVM = modelSVM.fit(X_train, y_train)
+    #X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.001 ,train_size=0.999, random_state=0)
+    modelSVM = modelSVM.fit(X, y)
     for i in modelSVM.predict(value):
-        if i>1:
+        if i>2:
             i = i+1
         else:
             i = i
